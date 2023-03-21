fde8e5cec791dbe726b5b2b206a99c2c36799699
Atticus Russell <atticusrussell@gmail.com>
2023-03-21 17:40:37 +0000

    commented, created example control ramping

--- ESCControl.py
+++ ESCControl.py
@@ -8,20 +8,17 @@ factory=PiGPIOFactory()
 ESC = AngularServo(13, min_angle=0, max_angle=100, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
 
 # initialize the ESC by running the calibration routine:
-# start at full throttle, hold for 2 sec
-ESC.angle(100)
-# TODO turn on the ESC here using the power button
-# put code here
-# hold full throttle for two seconds
-sleep(2)
-# throttle down. should now be calibrated	
-ESC.angle(0)
-# wait an extra moment before trying to do anything else
+ESC.angle=100 # start at full throttle
+# TODO turn on the ESC here using the power button spliced into GPIO
+sleep(2.2) # hold full throttle for two seconds (slight extra time for button)
+ESC.angle=0 # throttle down. should now be calibrated
+sleep(0.5) # wait a moment before anything else
 
+# loop infinitley ramping up throttle
 while (True):
-    ESC.angle=0
-    sleep(2)
-    ESC.angle=90
-    sleep(2)
-    ESC.angle=180
-    sleep(2)
+    ESC.angle=0 # throttle off
+    for i in range (15):
+        ESC.angle=i
+        print(i)
+        sleep(0.5) # wait a half second
+    sleep(1) # wait a full second 
