3e9f7b48f29e1a8751b12086bfb3644a6fb4322a
Atticus Russell <atticusrussell@gmail.com>
2023-03-21 02:42:49 +0000

    converted to angular adjustment

--- hwServoControl.py
+++ hwServoControl.py
@@ -1,17 +1,16 @@
-from gpiozero import Servo
+from gpiozero import AngularServo
 from time import sleep
 
 from gpiozero.pins.pigpio import PiGPIOFactory
 
 factory=PiGPIOFactory()
 # these servo params are tuned for the Spektrum s605 horizon hobby servo
-servo = Servo(18, min_pulse_width=0.65/1000, max_pulse_width=2.5/1000, pin_factory=factory)
+servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.65/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
 while (True):
-    servo.min()
+    servo.angle=0
     sleep(2)
-    servo.mid()
+    servo.angle=90
     sleep(2)
-    servo.max()
+    servo.angle=180
     sleep(2)
-    servo.value = None
