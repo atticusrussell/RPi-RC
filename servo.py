8787ac9c7b02df54876f90c12f61b5aacec16432
Atticus Russell <60147768+atticusrussell@users.noreply.github.com>
2023-04-03 21:20:59 -0400

    4 implement servo as functions (#5)
    * implemented cleanup
    
    *  set the angle of the servo using function call

--- servo.py
+++ servo.py
@@ -7,10 +7,32 @@ factory=PiGPIOFactory()
 # these servo params are tuned for the Spektrum s605 horizon hobby servo
 servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.65/1000, max_pulse_width=2.5/1000, pin_factory=factory)
 
-while (True):
-    servo.angle=0
-    sleep(2)
-    servo.angle=90
-    sleep(2)
-    servo.angle=180
-    sleep(2)
+
+def setAngle(angle):
+	servo.angle = angle
+
+
+def getAngle():
+	return servo.angle
+
+
+
+# if this isn't being called from another program
+if __name__ == '__main__':
+	try:
+		# little demo routine
+		while (True):
+			servo.angle=0
+			sleep(2)
+			servo.angle=90
+			sleep(2)
+			servo.angle=180
+			sleep(2)
+
+	except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+		print("Keyboard interrupt")
+
+	finally:
+		print("clean up") 
+		servo.detach()      # disable servo
+		print("Servo active?: ", servo.is_active)
