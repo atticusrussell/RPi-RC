0f70a28618833f9c6a079da4a3f669724b9049b5
Atticus Russell <atticusrussell@gmail.com>
2023-04-20 07:03:56 +0000

    fixed indentation

--- esc.cpp
+++ esc.cpp
@@ -26,51 +26,51 @@ Servo::Servo(int pin) {
     }
 }
 
-        int Servo::getPulseWidth() {
-            int rc = gpioGetServoPulsewidth(__pin);
-            if (rc == PI_BAD_USER_GPIO) {
-                throw "Invalid user GPIO pin Error!";
-            } else if (rc == PI_NOT_SERVO_GPIO) {
-                throw "Invalid servo GPIO Error!";
-            }
-            return rc;
-        }
+int Servo::getPulseWidth() {
+    int rc = gpioGetServoPulsewidth(__pin);
+    if (rc == PI_BAD_USER_GPIO) {
+        throw "Invalid user GPIO pin Error!";
+    } else if (rc == PI_NOT_SERVO_GPIO) {
+        throw "Invalid servo GPIO Error!";
+    }
+    return rc;
+}
 
-        void Servo::setPulseWidth(int pulseWidth){
-            int rc = gpioServo(__pin, pulseWidth);
-            if (rc == PI_BAD_USER_GPIO) {
-                throw "Invalid user GPIO pin Error!";
-            } else if (rc == PI_BAD_PULSEWIDTH) {
-                throw "Invalid pulsewidth Error!";
-            }
-        }
+void Servo::setPulseWidth(int pulseWidth){
+    int rc = gpioServo(__pin, pulseWidth);
+    if (rc == PI_BAD_USER_GPIO) {
+        throw "Invalid user GPIO pin Error!";
+    } else if (rc == PI_BAD_PULSEWIDTH) {
+        throw "Invalid pulsewidth Error!";
+    }
+}
 
-        AngularServo::AngularServo(int pin, float minAngle, float maxAngle, int minPulseWidthUs, int maxPulseWidthUs) : Servo(pin) {
-                    __minAngle = minAngle;
-                    __maxAngle = maxAngle;
-                    __minPulseWidthUs = minPulseWidthUs;
-                    __maxPulseWidthUs = maxPulseWidthUs;
-        }
+AngularServo::AngularServo(int pin, float minAngle, float maxAngle, int minPulseWidthUs, int maxPulseWidthUs) : Servo(pin) {
+            __minAngle = minAngle;
+            __maxAngle = maxAngle;
+            __minPulseWidthUs = minPulseWidthUs;
+            __maxPulseWidthUs = maxPulseWidthUs;
+}
 
-        void AngularServo::setAngle(float angle){
-            __angle = angle;
-            // make sure angle is within bounds
-            if (__angle < __minAngle){
-                __angle = __minAngle;
-            }
-            if (__angle > __maxAngle){
-                __angle = __maxAngle;
-            }
-            int pulseWidth = __minPulseWidthUs + (__angle - __minAngle) * (__maxPulseWidthUs - __minPulseWidthUs) / (__maxAngle - __minAngle);
+void AngularServo::setAngle(float angle){
+    __angle = angle;
+    // make sure angle is within bounds
+    if (__angle < __minAngle){
+        __angle = __minAngle;
+    }
+    if (__angle > __maxAngle){
+        __angle = __maxAngle;
+    }
+    int pulseWidth = __minPulseWidthUs + (__angle - __minAngle) * (__maxPulseWidthUs - __minPulseWidthUs) / (__maxAngle - __minAngle);
 
-            setPulseWidth(pulseWidth);
-        }
+    setPulseWidth(pulseWidth);
+}
 
-        int AngularServo::getAngle(){
-            int pulseWidth = getPulseWidth();
-            float angle = __minAngle + (pulseWidth - __minPulseWidthUs) * (__maxAngle - __minAngle) / (__maxPulseWidthUs - __minPulseWidthUs);
-            return angle;
-        }
+int AngularServo::getAngle(){
+    int pulseWidth = getPulseWidth();
+    float angle = __minAngle + (pulseWidth - __minPulseWidthUs) * (__maxAngle - __minAngle) / (__maxPulseWidthUs - __minPulseWidthUs);
+    return angle;
+}
 
 
 
@@ -102,92 +102,91 @@ ESC::ESC(int pwmPin, int fullRevThrottle, int fullFwdThrottle, int minPulseWidth
 //     cout << "ESC destructor finished" << endl;
 // }
 
-        void ESC::turnOn() {
-            cout << "Powering on ESC" << endl;
-            int rc = gpioWrite(__powerPin, 1);
-            if ( rc == PI_BAD_GPIO){
-                throw "Invalid GPIO pin Error!";
-            } else if ( rc == PI_BAD_LEVEL) {
-                throw "Inalid GPIO level Error!";
-            }
-            cout << "ESC on" << endl;
-        }
+void ESC::turnOn() {
+    cout << "Powering on ESC" << endl;
+    int rc = gpioWrite(__powerPin, 1);
+    if ( rc == PI_BAD_GPIO){
+        throw "Invalid GPIO pin Error!";
+    } else if ( rc == PI_BAD_LEVEL) {
+        throw "Inalid GPIO level Error!";
+    }
+    cout << "ESC on" << endl;
+}
 
-        void ESC::turnOff() {
-            cout << "Powering off ESC" << endl;
-            int rc = gpioWrite(__powerPin, 0);
-            if ( rc == PI_BAD_GPIO){
-                throw "Invalid GPIO pin Error!";
-            } else if ( rc == PI_BAD_LEVEL) {
-                throw "Inalid GPIO level Error!";
-            }
-            cout << "ESC off" << endl;
-        }
+void ESC::turnOff() {
+    cout << "Powering off ESC" << endl;
+    int rc = gpioWrite(__powerPin, 0);
+    if ( rc == PI_BAD_GPIO){
+        throw "Invalid GPIO pin Error!";
+    } else if ( rc == PI_BAD_LEVEL) {
+        throw "Inalid GPIO level Error!";
+    }
+    cout << "ESC off" << endl;
+}
 
-        void ESC::setThrottleRaw(double throttle) {
-            setAngle(throttle);
-            cout << "Throttle: " << throttle << " / ±" << __maxAngle << endl;
-        }
+void ESC::setThrottleRaw(double throttle) {
+    setAngle(throttle);
+    cout << "Throttle: " << throttle << " / ±" << __maxAngle << endl;
+}
 
-        double ESC::fixThrottle(double throttle) {
-            if (throttle > __neutralThrottle) {
-                throttle += __minFwdThrottle - 1;
-            } else if (throttle < __neutralThrottle) {
-                throttle += __minRevThrottle + 1;
-            }
+double ESC::fixThrottle(double throttle) {
+    if (throttle > __neutralThrottle) {
+        throttle += __minFwdThrottle - 1;
+    } else if (throttle < __neutralThrottle) {
+        throttle += __minRevThrottle + 1;
+    }
 
-            if (throttle > __maxAngle) {
-                throttle = __maxAngle;
-            } else if (throttle < __minAngle) {
-                throttle = __minAngle;
-            }
+    if (throttle > __maxAngle) {
+        throttle = __maxAngle;
+    } else if (throttle < __minAngle) {
+        throttle = __minAngle;
+    }
 
-            return throttle;
-        }
+    return throttle;
+}
 
-        void ESC::setThrottle(double throttle) {
-            throttle = fixThrottle(throttle);
-            setAngle(throttle);
-            cout << "Throttle: " << throttle << " / ±" << __maxAngle << endl;
+void ESC::setThrottle(double throttle) {
+    throttle = fixThrottle(throttle);
+    setAngle(throttle);
+    cout << "Throttle: " << throttle << " / ±" << __maxAngle << endl;
+}
 
-        }
 
+void ESC::calibrate() {
+    cout << "Calibrating:" << endl;
+    turnOff(); // make sure ESC is off just in case
+    cout << "ESC should start off" << endl;
+    cout << "Setting max throttle" << endl;
+    setThrottle(__maxAngle);
+    turnOn();
+    sleep(2); // hold full throttle for two seconds
+    cout << "Should hear two beeps" << endl;
+    sleep(1); // wait a second
+    cout << "Setting neutral throttle" << endl;
+    setThrottle(__neutralThrottle);
+    cout << "Should hear long beep" << endl;
+    sleep(2); // hold neutral throttle for two seconds
+    cout << "ESC should be calibrated" << endl;
+    cout << "Normal startup noises:" << endl;
+    cout << "First beeps: 3 for 3 cell battery, 4 for 4 cell" << endl;
+    sleep(1);
+    cout << "Second beeps: 1 for brake on, 2 for brake off" << endl;
+    sleep(1);
+    cout << "ESC startup done" << endl;
+}
 
-        void ESC::calibrate() {
-            cout << "Calibrating:" << endl;
-            turnOff(); // make sure ESC is off just in case
-            cout << "ESC should start off" << endl;
-            cout << "Setting max throttle" << endl;
-            setThrottle(__maxAngle);
-            turnOn();
-            sleep(2); // hold full throttle for two seconds
-            cout << "Should hear two beeps" << endl;
-            sleep(1); // wait a second
-            cout << "Setting neutral throttle" << endl;
-            setThrottle(__neutralThrottle);
-            cout << "Should hear long beep" << endl;
-            sleep(2); // hold neutral throttle for two seconds
-            cout << "ESC should be calibrated" << endl;
-            cout << "Normal startup noises:" << endl;
-            cout << "First beeps: 3 for 3 cell battery, 4 for 4 cell" << endl;
-            sleep(1);
-            cout << "Second beeps: 1 for brake on, 2 for brake off" << endl;
-            sleep(1);
-            cout << "ESC startup done" << endl;
-        }
-
-        void ESC::start() {
-            cout << "ESC starting up" << endl;
-            setThrottle(__neutralThrottle);
-            turnOn();
-            cout << "Listen to the ESC beeps now" << endl;
-            sleep(2);
-            cout << "First beeps: 3 for 3 cell battery, 4 for 4 cell" << endl;
-            sleep(2);
-            cout << "Second beeps: 1 for brake on, 2 for brake off" << endl;
-            sleep(2);
-            cout << "ESC startup done" << endl;
-        }
+void ESC::start() {
+    cout << "ESC starting up" << endl;
+    setThrottle(__neutralThrottle);
+    turnOn();
+    cout << "Listen to the ESC beeps now" << endl;
+    sleep(2);
+    cout << "First beeps: 3 for 3 cell battery, 4 for 4 cell" << endl;
+    sleep(2);
+    cout << "Second beeps: 1 for brake on, 2 for brake off" << endl;
+    sleep(2);
+    cout << "ESC startup done" << endl;
+}
 
 
 
